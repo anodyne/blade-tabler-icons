619bfafcc2ab71ff3fb99e5da8e46904b6861130
David VanScott <david.vanscott@gmail.com>
2024-03-11 15:32:23 -0400

    Fix tests

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -13,17 +13,11 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
-        $result = svg('tabler-2fa')->toHtml();
+        $result = svg('tabler-abc')->toHtml();
 
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<SVG
-        <svg class="tabler-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M7 16h-4l3.47 -4.66a2 2 0 1 0 -3.47 -1.54" />
-          <path d="M10 16v-8h4" />
-          <path d="M10 12l3 0" />
-          <path d="M17 16v-6a2 2 0 0 1 4 0v6" />
-          <path d="M17 13l4 0" />
-        </svg>
+        <svg class="tabler-icon" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="icon icon-tabler icons-tabler-outline icon-tabler-abc" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M3 16v-6a2 2 0 1 1 4 0v6M3 13h4M10 8v6a2 2 0 1 0 4 0v-1a2 2 0 1 0-4 0v1M20.732 12A2 2 0 0 0 17 13v1a2 2 0 0 0 3.726 1.01"/></svg>
         SVG;
 
         $this->assertStringMatchesFormat($result, $expected);
@@ -32,16 +26,10 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_classes_to_icons()
     {
-        $result = svg('tabler-2fa', 'w-6 h-6 text-gray-500')->toHtml();
+        $result = svg('tabler-abc', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<SVG
-        <svg class="tabler-icon w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M7 16h-4l3.47 -4.66a2 2 0 1 0 -3.47 -1.54" />
-          <path d="M10 16v-8h4" />
-          <path d="M10 12l3 0" />
-          <path d="M17 16v-6a2 2 0 0 1 4 0v6" />
-          <path d="M17 13l4 0" />
-        </svg>
+        <svg class="tabler-icon w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="icon icon-tabler icons-tabler-outline icon-tabler-abc" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M3 16v-6a2 2 0 1 1 4 0v6M3 13h4M10 8v6a2 2 0 1 0 4 0v-1a2 2 0 1 0-4 0v1M20.732 12A2 2 0 0 0 17 13v1a2 2 0 0 0 3.726 1.01"/></svg>
         SVG;
 
         $this->assertStringMatchesFormat($expected, $result);
@@ -50,16 +38,10 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_styles_to_icons()
     {
-        $result = svg('tabler-2fa', ['style' => 'color: #555'])->toHtml();
+        $result = svg('tabler-abc', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<SVG
-        <svg style="color: #555" class="tabler-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M7 16h-4l3.47 -4.66a2 2 0 1 0 -3.47 -1.54" />
-          <path d="M10 16v-8h4" />
-          <path d="M10 12l3 0" />
-          <path d="M17 16v-6a2 2 0 0 1 4 0v6" />
-          <path d="M17 13l4 0" />
-        </svg>
+        <svg style="color: #555" class="tabler-icon" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="icon icon-tabler icons-tabler-outline icon-tabler-abc" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M3 16v-6a2 2 0 1 1 4 0v6M3 13h4M10 8v6a2 2 0 1 0 4 0v-1a2 2 0 1 0-4 0v1M20.732 12A2 2 0 0 0 17 13v1a2 2 0 0 0 3.726 1.01"/></svg>
         SVG;
 
         $this->assertStringMatchesFormat($expected, $result);
