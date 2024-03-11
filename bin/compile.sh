af19550454e079a62a69a6b7b1da4c57b554da5c
David VanScott <david.vanscott@gmail.com>
2024-03-11 15:05:41 -0400

    Updates

--- bin/compile.sh
+++ bin/compile.sh
@@ -6,10 +6,30 @@ DIRECTORY=$(cd $1 && pwd)
 DIST=$DIRECTORY
 RESOURCES=$DIRECTORY/../../../../resources/svg
 
-echo "Compiling icons..."
+echo "Compiling filled icons..."
 
-for FILE in $DIST/*; do
-    sed -e 's/ class="[^\"]*"//g' -e '/<path stroke="none" d="M0 0h24v24H0z" fill="none"\/>/d' -e '/^$/d' $FILE > $RESOURCES/$(echo ${FILE##*/})
+for FILE in $DIST/filled/*; do
+    file="${FILE%/}";
+    prefix="/Users/dvs/Code/personal/blade-tabler-icons/node_modules/@tabler/icons/icons/filled/"
+
+    if [[ "$file" =~ ^($prefix)(.*)(.svg)$ ]]; then
+        filename="${BASH_REMATCH[2]}"
+        echo filename: $filename
+        sed -e 's/width="24"//g;s/height="24"//g' -e '/^$/d' $file > "$RESOURCES/$filename-filled.svg"
+    fi
+done
+
+echo "Compiling outline icons..."
+
+for FILE in $DIST/outline/*; do
+    file="${FILE%/}";
+    prefix="/Users/dvs/Code/personal/blade-tabler-icons/node_modules/@tabler/icons/icons/outline/"
+
+    if [[ "$file" =~ ^($prefix)(.*)$ ]]; then
+        filename="${BASH_REMATCH[2]}"
+        echo filename: $filename
+        sed -e '/^$/d' $file > "$RESOURCES/$filename"
+    fi
 done
 
 echo "All done!"
